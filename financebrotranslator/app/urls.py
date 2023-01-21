1b596b409942a1a9f1504ece686e0f6c34b10d25
Ginevs <49313437+Ginevs@users.noreply.github.com>
2023-01-21 17:00:47 -0500

    fix admin url

--- financebrotranslator/app/urls.py
+++ financebrotranslator/app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.index, name="home-page"),
 ]
