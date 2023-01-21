a3935e7f8680fe9780e8b115a274718213088dd8
Thomas Hart <thomas.hart@uwaterloo.ca>
2023-01-21 04:43:23 -0500

    increase textarea size

--- financebrotranslator/app/forms.py
+++ financebrotranslator/app/forms.py
@@ -1,11 +1,10 @@
 from django import forms
 
 class TranslateForm(forms.Form):
-    text = forms.CharField(widget=forms.Textarea(attrs={"rows":"5", "cols":"40"}))
+    text = forms.CharField(widget=forms.Textarea(attrs={"rows":"25", "cols":"100"}))
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for key, field in self.fields.items():
             field.label = ""
             field.required = False
-            
